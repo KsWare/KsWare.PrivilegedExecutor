2be114ff937ae08a1c156136a52a253ca8dc01eb
SchreinerK <ks@ksware.de>
2018-02-14 22:32:53 +0100

    .

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,26 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8b3fbd46-105f-414b-9c61-053c6dad0833")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
 
 [assembly: InternalsVisibleTo("KsWare.PrivilegedExecutor.Tests")]
 
